5ef4bda5c4eefff649e83bb881c491fe5587b229
namgivu <namgivu@gmail.com>
2017-08-26 18:14:18 +0700

    util @ install node.js npm

---
+++ util/01.install-npm-react.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#install npm i.e. node.js package manager
+sudo apt install -y npm
+
+#on ubuntu 16.04, require this symlink ref. https://stackoverflow.com/a/20890795/248616
+sudo ln -s /usr/bin/nodejs /usr/bin/node

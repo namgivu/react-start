acc7ea1c9613d1b9a4db570c9dc42e186276c3ed
namgivu <namgivu@gmail.com>
2017-08-28 11:00:23 +0700

    util @ start app

--- util/00.start-web-server.sh
+++ util/00.start-web-server.sh
@@ -5,15 +5,19 @@ s=$BASH_SOURCE ; s=$(dirname "$s") ; s=$(cd "$s" && pwd) ; export SCRIPT_HOME=$s
 
 source "$SCRIPT_HOME/common.sh"
 
+#START_FOLDER=$REACT_STARTER_KIT
+START_FOLDER=$APP_HOME
+
 #print guideline
 echo "
 React Starter Kit is now ready at
 http://localhost:$PORT
 this serves the sample web pages located at
-$REACT_STARTER_KIT
+$START_FOLDER
 "
 
 #start the web server
-cd $REACT_STARTER_KIT
+#cd $REACT_STARTER_KIT
+cd $START_FOLDER
 python -m SimpleHTTPServer $PORT
 cd --
